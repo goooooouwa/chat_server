a3feae2c83f309522839f08883fbe5d7e0cf46df
Greg <greg.xu@activenetwork.com>
2015-11-03 15:35:35 +0800

    integrate Redis and subscribe to a channel
    Ref: http://liamkaufman.com/blog/2013/02/27/adding-real-time-to-a-restful-rails-app/

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var redis = require('redis').createClient();
+
+redis.subscribe('rt-change');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -12,6 +15,10 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     io.emit('chat message', msg);
+  });
+
+  redis.on('message', function(channel, message){
+    socket.emit('rt-change', message);
   });
 
   socket.on('disconnect', function(){
