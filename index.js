53eb45865f61dc57e0bcb7cfa690e175edeaccf1
Greg <greg.xu@activenetwork.com>
2015-11-10 12:32:06 +0800

    modify config to fit Docker setup, but NOT WORKING

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var redis = require('redis').createClient();
+var redis = require('redis').createClient(6379, 'redis');
 
 redis.subscribe('data-change');
 
 app.get('/', function(req, res){
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.sendFile(__dirname + '/index.html');
 });
 
